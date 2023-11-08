fc30fdc782e6dcd0a3ef2f2ac2a7fe5195d1f266
smyler3 <mylertalym01@gmail.com>
2023-11-08 14:41:43 +1100

    Add form submission logic
    Submit button now linked to form, a series of checks are now performed
    on submission and form submit will refresh page

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const MIN_PASSWORD_LENGTH = 8;
 
+/* 
+ *------------------------------------------------------------------------------------------------
+ * Functions for initialising password information
+ *------------------------------------------------------------------------------------------------
+ */
+
 // Initialise all functions related to password setup
 function setupPasswordFunctionality() {
     setPasswordMinLength();
@@ -31,30 +37,68 @@ function setupPasswordTooltip() {
         passFields[i]
     }
     console.log(passFields);
-
 }
 
-function createClickEvents() {
-    // Creating form submission button
-    const submitBtn = document.querySelector('.sign-up-btn');
-    submitBtn.addEventListener('click', checkPasswordsmatch);
+/* 
+ *------------------------------------------------------------------------------------------------
+ * Functions for checking password input
+ *------------------------------------------------------------------------------------------------
+ */
+
+ function validateForm() {
+    const password = document.querySelector('#password');
+    const passwordConfirm = document.querySelector('#cpassword');
+
+    return (checkPasswordsmatch(password, passwordConfirm) && checkPasswordLength(password, passwordConfirm));
 }
 
 // Checks that the password and confirm password fields match
-function checkPasswordsmatch() {
-    const password = document.querySelector('#password').value;
-    const passwordConfirm = document.querySelector('#cpassword').value;
+function checkPasswordsmatch(password, passwordConfirm) {
+    const passwordField = document.querySelector('#password-field');
 
-    console.log(password);
-    console.log(passwordConfirm);
+    console.log(password.value);
+    console.log(passwordConfirm.value);
 
-    if (password === passwordConfirm) {
+    if (password.value === passwordConfirm.value) {
         console.log(true);
+        removeVisualPasswordError(password, passwordConfirm, passwordField);
+        removePasswordTooltip();
         return true;
     }
     console.log(false);
+    setVisualPasswordError(password, passwordConfirm, passwordField);
     return false;
 }
 
-createClickEvents();
+// Checks that the password fields are above the minimum length
+function checkPasswordLength(password, passwordConfirm) {
+    return (password.length >= MIN_PASSWORD_LENGTH && passwordConfirm.length >= MIN_PASSWORD_LENGTH); 
+}
+
+/* 
+ *------------------------------------------------------------------------------------------------
+ * Functions for altering password visuals
+ *------------------------------------------------------------------------------------------------
+ */
+
+// Visually indicate to user passwords don't match
+function setVisualPasswordError(password, passwordConfirm, passwordField) {
+    passwordField.classList.add('input-error');
+    password.classList.add('input-error');
+    passwordConfirm.classList.add('input-error');
+}
+
+// Visually indicate to user passwords don't match
+function removeVisualPasswordError(password, passwordConfirm, passwordField) {
+    passwordField.classList.remove('input-error');
+    password.classList.remove('input-error');
+    passwordConfirm.classList.remove('input-error');
+}
+
+/* 
+ *------------------------------------------------------------------------------------------------
+ * Main
+ *------------------------------------------------------------------------------------------------
+ */
+
 setupPasswordFunctionality();
